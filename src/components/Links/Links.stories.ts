9f9b66128ff0a505820a1a888c751794439afc47
Stefany Repetcki <stefany.c.r@hotmail.com>
2022-03-15 15:51:35 -0300

    feat(Componente): Links.stories

---
+++ src/components/Links/Links.stories.ts
@@ -0,0 +1,101 @@
+export default {
+	title: 'DOCUMENTAÇÃO/Links Criativos e Técnicos',
+};
+
+const Template = () => ({
+	template: `
+	<div class="d-flex justify-content-center">
+		<ul>
+			<h1 class="font-weight-bold">FRONT END - LINKS CRIATIVOS E TÉCNICOS</h1>
+			<br><br>
+			<h3 class="font-weight-bold">LINKS PARA INSPIRAÇÕES</h3>
+			<li class="font-weight-bold"><a href="https://www.lapa.ninja" target="_blank">Landing Pages https://www.lapa.ninja/</a></li>
+			<li class="font-weight-bold"><a href="https://www.behance.net" target="_blank">https://www.behance.net/</a></li>
+			<li class="font-weight-bold"><a href="https://dribbble.com/" target="_blank">https://dribbble.com/</a></li>
+			<li class="font-weight-bold"><a href="https://www.graphicsfuel.com/" target="_blank">https://www.graphicsfuel.com/<a/></li>
+			<li class="font-weight-bold"><a href="https://www.deviantart.com/" target="_blank">https://www.deviantart.com/<a/></li>
+			<li class="font-weight-bold"><a href="https://br.pinterest.com/" target="_blank">https://br.pinterest.com/</a></li>
+			<li class="font-weight-bold"><a href="https://themeforest.net/item/thefox-multipurpose-psd-template/screenshots/8894353?index=39" target="_blank">https://themeforest.net/item/thefox-multipurpose-psd-template/screenshots/8894353?index=39</a></li>
+			<br><br>
+			<h3 class="font-weight-bold">LINKS TÉCNICOS E ETC.</h3>
+			<li class="font-weight-bold"><a href="https://www.flaticon.com" target="_blank">icons https://www.flaticon.com/</a></li>
+			<li class="font-weight-bold"><a href="https://www.gradientmagic.com" target="_blank">gradientes https://www.gradientmagic.com</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">PAGINA LENTA DE IMAGENS TESTE https://imagekit.io/website-analyzer</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">PNG2JPG PARA CONVERTER</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Imagem antes e depois: https://codepen.io/karlen/pen/JXBaEe </a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">IMAGE HOVER EFFECTS: https://miketricking.github.io/bootstrap-image-hover/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">roda de cores para combinações https://color.adobe.com/pt/create</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Menus diferentes https://blog.codepen.io/2018/07/01/codepenchallenge-menus-roundup/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Transform CSS Generator: Scale, Rotate, Translate and Skew https://html-css-js.com/css/generator/transform/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Responsive Hamburger Menu CSS Examples https://www.digitaldesignjournal.com/best-hamburger-menu-css/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Organic Shape Animations with SVG clipPath https://tympanus.net/codrops/2017/06/28/organic-shape-animations-with-svg-clippath/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Css clik path make https://bennettfeely.com/clippy/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">https://css-tricks.com/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">https://www.littlesnippets.net/blog/some-css-menu-inspiration-using-animated-lines https://miketricking.github.io/bootstrap-image-hover/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">https://codepen.io/tiffachoo/pen/BaQpVeR 	</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">https://getcssscan.com/css-box-shadow-examples</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">grapesjs https://www.youtube.com/watch?v=HnDxpG2Sc2s </a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">https://www.svgbackgrounds.com/ </a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">https://bgjar.com/</a></li>
+			<br><br>
+			<h3 class="font-weight-bold">FRELAS:</h3>
+			<li class="font-weight-bold"><a href="" target="_blank">https://br.fiverr.com/start_selling?source=top_nav</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">https://freelancer.com.br/freelancers/s/frontend?category=3</a></li>
+			<br><br>
+			<h3 class="font-weight-bold">ANIMAÇÕES</h3>
+			<li class="font-weight-bold"><a href="" target="_blank">Animate https://daneden.github.io/animate.css/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Animsta https://animista.net/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Tilomitra https://tilomitra.github.io/infinite/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Motion UI https://zurb.com/playground/motion-ui</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Micron Js https://webkul.github.io/micron/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Vivify http://vivify.mkcreative.cz/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">HOver https://ianlunn.github.io/Hover/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">All Animation https://all-animation.github.io/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Minimamente https://www.minimamente.com/project/magic/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Its Tuesday https://shakrmedia.github.io/tuesday/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">RebaundGen https://dwarcher.github.io/reboundgen/examples/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Css Shake https://elrumordelaluz.github.io/csshake/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Motion CSS http://pavlyukpetr.github.io/motion-css//#</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Let it rip https://kristofferandreasen.github.io/wickedCSS/#</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Woah.css http://www.joerezendes.com/projects/Woah.css/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">OBNOXIOUS.CSS http://tholman.com/obnoxious/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Mimic https://codepen.io/etreacy/pen/ZJYoRV</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">border-radius generator https://border-radius.com/</a></li>
+			<br><br>
+			<h3 class="font-weight-bold">LINKS	 BANCOS DE IMAGENS:</h3>
+			<li class="font-weight-bold"><a href="" target="_blank">https://www.stockfreeimages.com/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">http://www.freeimages.co.uk/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">http://www.freedigitalphotos.net/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">https://pt.freeimages.com/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">https://pixabay.com/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">http://www.everystockphoto.com/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">https://www.publicdomainpictures.net/en/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Free videos clipes: https://mazwai.com/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">editor de fotos (photoshop) online: https://www.photopea.com/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">remove background imagens: https://www.remove.bg/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">Startup free photos: https://startupstockphotos.com/</a></li>
+			<li class="font-weight-bold"><a href="" target="_blank">https://unsplash.com </a></li>
+			<br><br>
+			<h3 class="font-weight-bold">TEMPLATE CSS/HTML (DOWNLOAD)</h3>
+			<li class="font-weight-bold"><a href="https://colorlib.com/demo?theme=labs" target="_blank">Labs: https://colorlib.com/demo?theme=labs</a></li>
+			<li class="font-weight-bold"><a href="https://bootstrapmade.com/demo/Reveal/" target="_blank">Reveal: https://bootstrapmade.com/demo/Reveal/</a></li>
+			<li class="font-weight-bold"><a href="https://www.stocklayouts.com/" target="_blank">Stock Layout: https://www.stocklayouts.com/</a></li>
+			<li class="font-weight-bold"><a href="https://www.pixeden.com/" target="_blank">Premium Design & Web Resources: https://www.pixeden.com/</a></li>
+			<br><br>
+			<li class="font-weight-bold"><a href="https://www.youtube.com/audiolibrary/music?nv=1" target="_blank">biblioteca de audios free do youtube: https://www.youtube.com/audiolibrary/music?nv=1 </a></li>
+			<br><br>
+			<h3 class="font-weight-bold">ARQUIVOS BOOTSTRAP FIGMA:</h3>
+			<li class="font-weight-bold"><a href="https://www.figma.com/file/l8iPIZp2KoznTKXPDarDzB/Layout-Grids?node-id=57%3A21<" target="_blank">Grid: https://www.figma.com/file/l8iPIZp2KoznTKXPDarDzB/Layout-Grids?node-id=57%3A21</a></li>
+			<li class="font-weight-bold"><a href="https://www.figma.com/file/SGJIUhVFFrTtMQALe774jP/Bootstrap-v4-uikit-Copy?node-id=0%3A1" target="_blank">Componentes: https://www.figma.com/file/SGJIUhVFFrTtMQALe774jP/Bootstrap-v4-uikit-Copy?node-id=0%3A1</a></li>
+			<br><br>
+			<li class="font-weight-bold"><a href="https://github.com/commitizen/cz-cli" target="_blank">commit cz-cli https://github.com/commitizen/cz-cli</a></li>
+			<li class="font-weight-bold"><a href="https://github.com/conventional-changelog/conventional-changelog" target="_blank">https://github.com/conventional-changelog/conventional-changelog</a></li>
+			<li class="font-weight-bold">Husky para prevenir commits ruins</li>
+			<li class="font-weight-bold"><a href="https://www.conventionalcommits.org/en/v1.0.0/" target="_blank">commits semanticos https://www.conventionalcommits.org/en/v1.0.0/ </a></li>
+			<li class="font-weight-bold"><a href="https://github.com/angular/angular/blob/22b96b9/CONTRIBUTING.md#-commit-message-guidelines" target="_blank">https://github.com/angular/angular/blob/22b96b9/CONTRIBUTING.md#-commit-message-guidelines</a></li>
+			<br><br>
+		</ul>
+	</div>`
+});
+
+export const Links = Template.bind({});
