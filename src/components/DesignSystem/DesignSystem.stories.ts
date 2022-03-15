99bda5f93d334d6e4aafe9adea9616bb29484810
Stefany Repetcki <stefany.c.r@hotmail.com>
2022-03-15 15:58:06 -0300

    feat(Componente): DesignSystem.stories

---
+++ src/components/DesignSystem/DesignSystem.stories.ts
@@ -0,0 +1,21 @@
+export default {
+	title: 'DESIGN SYSTEM/Cores',
+};
+
+export const Cores = () => ({
+	template: `
+<div class="container">
+  <h1>Backgrounds</h1>
+  <hr>
+  <div class="p-3 mb-2 bg-primary text-white">.bg-primary</div>
+  <div class="p-3 mb-2 bg-secondary text-white">.bg-secondary</div>
+  <div class="p-3 mb-2 bg-success text-white">.bg-success</div>
+  <div class="p-3 mb-2 bg-danger text-white">.bg-danger</div>
+  <div class="p-3 mb-2 bg-warning text-dark">.bg-warning</div>
+  <div class="p-3 mb-2 bg-info text-white">.bg-info</div>
+  <div class="p-3 mb-2 bg-light text-dark">.bg-light</div>
+  <div class="p-3 mb-2 bg-dark text-white">.bg-dark</div>
+  <div class="p-3 mb-2 bg-white text-dark">.bg-white</div>
+  <div class="p-3 mb-2 bg-transparent text-dark">.bg-transparent</div>
+</div>`,
+});
