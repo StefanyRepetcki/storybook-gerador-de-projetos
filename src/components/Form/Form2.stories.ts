5be6783433a3c66d5e4bc5ef5b5696c1e3822812
Stefany Repetcki <stefany.c.r@hotmail.com>
2022-03-15 15:56:26 -0300

    feat(Componente): Form2.stories

---
+++ src/components/Form/Form2.stories.ts
@@ -0,0 +1,40 @@
+export default {
+  title: 'COMPONENTES/Form',
+};
+
+export const Form2 = () => ({
+  template: `<section id="last">
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-8 col-lg-offset-2 text-xs-center">
+                  <h2 class="margin-top-0 wow fadeIn">Entrar em contato</h2>
+                  <hr class="primary">
+                  <p>Adoramos o feedback. Preencha o formulário abaixo e retornaremos o mais breve possível.</p>
+              </div>
+              <div class="col-lg-10 col-lg-offset-1 text-xs-center">
+                  <form class="contact-form row">
+                      <div class="col-md-4">
+                          <label></label>
+                          <input type="text" class="form-control" placeholder="Nome">
+                      </div>
+                      <div class="col-md-4">
+                          <label></label>
+                          <input type="text" class="form-control" placeholder="E-mail">
+                      </div>
+                      <div class="col-md-4">
+                          <label></label>
+                          <input type="text" class="form-control" placeholder="Telefone">
+                      </div>
+                      <div class="col-md-12">
+                          <label></label>
+                          <textarea class="form-control" rows="9" placeholder="Sua mensagem aqui.."></textarea>
+                      </div>
+                      <div class="d-flex justify-content-center">
+                          <button type="button" class="mt-3 btn btn-primary w-50">Enviar</button>
+                      </div>
+                  </form>
+              </div>
+          </div>
+      </div>
+  </section>`,
+});
