75eda83d645305ddbb49aeb965e29910d0e5dfb0
Stefany Repetcki <stefany.c.r@hotmail.com>
2022-03-15 15:59:59 -0300

    feat(Componente): Button.stories

---
+++ src/components/Button/Button.stories.ts
@@ -0,0 +1,25 @@
+export default {
+	title: 'COMPONENTES/Button',
+	argTypes: { onClick: { action: 'clicked' } }
+};
+
+export const Button = (args: any, { argTypes }: any) => ({
+	template: `
+<div class="container">
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
+  <button type="button" class="btn btn-link">Link</button>
+  <br/>
+  <div class="mt-5">
+    <button type="button" class="btn btn-custom">Primary</button>
+    <button type="button" class="btn btn-custom-pink">Primary</button>
+    <button type="button" class="btn btn-custom-gradient">Primary</button>
+  </div>
+</div>`
+});
